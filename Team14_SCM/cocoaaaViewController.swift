db0e3ae6d0d186a1889fa939c8dcc6a411215dd0
Aviral Singh <149692321+the-number-17@users.noreply.github.com>
2024-12-02 11:54:37 +0530

    Initial Commit

---
+++ Team14_SCM/cocoaaaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  cocoaaaViewController.swift
+//  Team14_SCM
+//
+//  Created by Batch - 1 on 02/12/24.
+//
+
+import UIKit
+
+class cocoaaaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
